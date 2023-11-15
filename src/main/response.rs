3ee907f3f6679d9a1cffb5fa9a74ae83470628e3
Adrian John <adrianjohn6543@gmail.com>
2023-11-15 22:34:34 +0100

    add response and status structs

---
+++ src/main/response.rs
@@ -0,0 +1,10 @@
+use std::collections::BTreeMap;
+use crate::status::HttpStatus;
+use crate::version::HttpVersion;
+
+pub struct Response{
+    version: HttpVersion,
+    status: HttpStatus,
+    headers: BTreeMap<String,String>,
+    body: String
+}

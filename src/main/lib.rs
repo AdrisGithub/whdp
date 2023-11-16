2eef7046c7c19972560eb9e129647e0a765e5294
Adrian John <adrianjohn6543@gmail.com>
2023-11-16 23:15:05 +0100

    remove useless unit test

--- src/main/lib.rs
+++ src/main/lib.rs
@@ -1,27 +1,7 @@
-pub mod method;
 pub mod error;
-pub mod version;
+pub mod method;
 pub mod request;
 pub mod response;
 pub mod status;
-
-#[cfg(test)]
-mod tests {
-    use std::fs::File;
-    use std::io::Read;
-
-    use crate::request::Request;
-
-    #[test]
-    fn test_request() {
-        let mut file = File::open("src/resources/request.txt").unwrap();
-        let mut string = String::new();
-        let _ = file.read_to_string(&mut string);
-        let req = Request::try_from(string.as_bytes()).unwrap();
-        println!("{}", req.get_body());
-        println!("{}", req.get_method());
-        println!("{}", req);
-        println!("{:?}", req.get_headers().get("Host"));
-        let req = Request::try_from(string.as_bytes()).unwrap();
-    }
-}
+pub mod util;
+pub mod version;

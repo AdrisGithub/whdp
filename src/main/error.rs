cd3b4b9ed9adf68141583c300f49d8f6155bd06a
AdrisGithub <adrianjohn6543@gmail.com>
2023-11-16 14:18:44 +0100

    add better Error Type

--- src/main/error.rs
+++ src/main/error.rs
@@ -1,15 +1,40 @@
 use std::fmt::{Debug, Display, Formatter};
 use std::io::Error;
 
-const MESSAGE: &str = "Failure at parsing";
+const MESSAGES: [&str; 3] = [
+    "Unknown Failure at parsing the Request",
+    "Failure at a IO operation",
+    "Failure at parsing the Http Method"
+];
 
-// TODO better error messages
+#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash, Debug)]
+pub enum ParseErrorKind {
+    Unkown,
+    IO,
+    Method,
+}
+
+impl Display for ParseErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}: {}", self, MESSAGES[*self as usize])
+    }
+}
+
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash)]
-pub struct HttpParseError(());
+pub struct HttpParseError(ParseErrorKind);
 
 impl HttpParseError {
     pub const fn new() -> Self {
-        Self(())
+        Self(ParseErrorKind::Unkown)
+    }
+    pub const fn get_kind(&self) -> &ParseErrorKind {
+        &self.0
+    }
+}
+
+impl From<ParseErrorKind> for HttpParseError {
+    fn from(value: ParseErrorKind) -> Self {
+        Self(value)
     }
 }
 
@@ -22,7 +47,7 @@ impl Default for HttpParseError {
 
 impl Debug for HttpParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(MESSAGE)
+        f.write_str(&self.0.to_string())
     }
 }
 
@@ -31,7 +56,8 @@ impl Display for HttpParseError {
         Debug::fmt(self, f)
     }
 }
-impl From<Error> for HttpParseError{
+
+impl From<Error> for HttpParseError {
     fn from(_value: Error) -> Self {
         HttpParseError::new()
     }

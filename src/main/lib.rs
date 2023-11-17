df89f538cea7ffa870653cedbb4799e1df2b173f
AdrisGithub <adrianjohn6543@gmail.com>
2023-11-17 12:32:40 +0100

    reexport modules to make it more manageable

--- src/main/lib.rs
+++ src/main/lib.rs
@@ -1,3 +1,15 @@
+pub use error::HttpParseError;
+pub use error::ParseErrorKind;
+pub use method::HttpMethod;
+pub use request::Request;
+pub use request::TryRequest;
+pub use response::Response;
+pub use status::HttpStatus;
+pub use status::HttpStatusGroup;
+pub use status::presets;
+pub use util::Destruct;
+pub use version::HttpVersion;
+
 pub mod error;
 pub mod method;
 pub mod request;

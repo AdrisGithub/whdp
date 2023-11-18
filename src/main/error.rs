b11bf5e2932737b8227ebec141388605a8de6fd1
Adrian John <adrianjohn6543@gmail.com>
2023-11-18 16:33:03 +0100

    add documentation for error.rs

--- src/main/error.rs
+++ src/main/error.rs
@@ -1,29 +1,17 @@
 use std::fmt::{Debug, Display, Formatter};
 
-use crate::error::ParseErrorKind::Unkown;
 use crate::util::Destruct;
 
 const MESSAGE: &str = "Failure:";
 const UNKNOWN_MSG: &str = "Something unexpected went wrong";
 
-#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash, Debug)]
-pub enum ParseErrorKind {
-    Unkown,
-    IO,
-    Method,
-    Version,
-    Req,
-    Status,
-    Util,
-    Resp,
-}
-
-impl Display for ParseErrorKind {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
+/// ### Error struct for HTTP Parsing
+///
+/// contains a [kind] for automatically handling the error <br>
+/// and an optional [message] for further information
+///
+/// [kind]: crate::HttpParseError::get_kind
+/// [message]: crate::HttpParseError::get_msg
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Hash)]
 pub struct HttpParseError {
     kind: ParseErrorKind,
@@ -31,19 +19,24 @@ pub struct HttpParseError {
 }
 
 impl HttpParseError {
+    /// constructs a new instance of HttpParseError
+    /// with default values
     pub const fn new() -> Self {
         Self {
-            kind: Unkown,
+            kind: ParseErrorKind::Unkown,
             msg: None,
         }
     }
+    /// get the [ParseErrorKind] of this Error
     pub const fn get_kind(&self) -> &ParseErrorKind {
         &self.kind
     }
+    /// change the Error to have this message
     pub fn with_msg(&mut self, str: String) -> &mut Self {
         self.msg = Some(str);
         self
     }
+    /// get the Message of this Error
     pub fn get_msg(&self) -> String {
         self.msg.clone().unwrap_or(UNKNOWN_MSG.into())
     }
@@ -94,5 +87,45 @@ impl Destruct for HttpParseError {
     type Item = (ParseErrorKind, String);
     fn destruct(self) -> Self::Item {
         (self.kind, self.get_msg())
+    }
+}
+
+/// #### Enum for the different places where the parsing could went wrong
+/// This is more for error handling in match cases. It's used in [HttpParseError] <br>
+/// For genuine Information where it went wrong read the message
+#[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash, Debug)]
+pub enum ParseErrorKind {
+    /// Error type for [Default] Error
+    Unkown,
+    /// Error type for everything that has to do with parsing the [HttpMethod]
+    ///
+    /// [HttpMethod]: crate::HttpMethod
+    Method,
+    /// Error type for everything that has to do with parsing the [HttpVersion]
+    ///
+    /// [HttpVersion]: crate::HttpVersion
+    Version,
+    /// Error type for everything that has to do with parsing the [HttpRequest]
+    ///
+    /// [HttpRequest]: crate::Request
+    Req,
+    /// Error type for everything that has to do with parsing the [HttpStatus]
+    ///
+    /// [HttpStatus]: crate::HttpStatus
+    Status,
+    /// Error type for everything that has to do with parsing the [HttpResponse]
+    ///
+    /// [HttpResponse]: crate::Response
+    Resp,
+    /// Error type for some util functions for example
+    /// 1. Parsing the body
+    /// 2. Parsing the headers
+    /// 3. Parsing the uri
+    Util,
+}
+
+impl Display for ParseErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(self, f)
     }
 }
